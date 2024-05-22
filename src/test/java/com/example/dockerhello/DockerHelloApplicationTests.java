350cc39c418c0dd25ffa2a9284f25db001d37e50
Anida Assane Yacouba <aniyacoub18@gmail.com>
2024-05-22 11:23:43 +0000

    first commit

---
+++ src/test/java/com/example/dockerhello/DockerHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerhello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerHelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
